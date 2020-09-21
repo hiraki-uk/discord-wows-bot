173906170ca35816cf1407bbe913e318d25ff146
hiraki.uk <hiraki.uk@gmailcom>
2020-09-21 14:37:28 +0100

    startup on slot method

--- cogs/cogs.py
+++ cogs/cogs.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 import discord
 from discord.ext import commands, tasks
@@ -15,9 +16,9 @@ class Cogs(commands.Cog):
 	__slots__ = ('bot', 'logger')
 	
 	"""	基本的な命令をここに格納してるよ！ """
-	def __init__(self, bot, logger=None):
+	def __init__(self, bot):
 		self.bot = bot
-		self.logger = Logger(__name__) if logger is None else logger
+		self.logger = Logger(__name__)
 
 	# version
 	@commands.command()
@@ -38,8 +39,51 @@ class Cogs(commands.Cog):
 		""" 直近5メッセージを削除するよ！ """
 		async for message in ctx.channel.history(limit=6):
 			await message.delete()
-	
+
+	# server status
 	@commands.command()
-	async def test(self, ctx):
-		for member in ctx.guild.members:
-			print(member.activities)
+	async def vmstat(self, ctx):
+		""" みつばサーバーの負荷状況を教えるよ！ """
+		stats = exec('vmstat')
+		await ctx.send('``` \
+			stats \
+		````')
+		
+	# urls
+	@commands.command()
+	async def jyantama(self, ctx):
+		""" じゃんたまのURLだよ～ """
+		mes = ':thumbsup:\nhttps://game.mahjongsoul.com/'
+		await ctx.send(mes)
+	@commands.command()
+	async def numbers(self, ctx):
+		""" statsのURLだよ～ """
+		mes = ':thumbsup:\nhttps://asia.wows-numbers.com/'
+		await ctx.send(mes)
+	@commands.command()
+	async def hp(self, ctx):
+		""" wowsホームページのURLだよ～"""
+		mes = ':thumbsup:\nhttps://worldofwarships.asia/'
+		await ctx.send(mes)
+	@commands.command()
+	async def zeal(self, ctx):
+		""" ZEALホームページのURLだよ～ """
+		mes = ':thumbsup:\nhttps://zeal.live-on.net'
+		await ctx.send(mes)
+
+	@commands.command()
+	async def slot(self, ctx):
+		""" ランク戦味方占い！"""
+		with open('res/rank_prefix.txt', 'r') as f:
+			prefix = f.read()
+		indexes = random.random(len(prefix), 6) # index of your teammates
+		prefixes = [prefix[i] for i in range(indexes)]
+		ships = []
+		sol = [prefix[i] + ships[i] for i in range(indexes)]
+
+		await ctx.send('\n'.join(sol))
+
+
+
+		
+		
